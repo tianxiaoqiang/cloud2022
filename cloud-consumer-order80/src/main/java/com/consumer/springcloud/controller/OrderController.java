0fbe477f2102ea618806ee5465ac500496bfc8c4
田小强 <tianminqiang@163.com>
2022-03-17 11:54:09 +0800

    getForEntity

--- cloud-consumer-order80/src/main/java/com/consumer/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/consumer/springcloud/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,16 @@ public class OrderController {
 
         log.error("paymentUrl:"+paymentUrl);
         return restTemplate.getForObject(paymentUrl + "/payment/get/" + id, CommonResult.class);
+    }
+
+    //getForEntity()
+    @GetMapping("/consumer/paymentList/get/{id}")
+    public CommonResult<Payment> getPaymentList(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(paymentUrl + "/payment/get/" + id, CommonResult.class);
+        if(forEntity.getStatusCode().is2xxSuccessful()){
+            return forEntity.getBody();
+        }else{
+            return new CommonResult<>(200,"暂无信息",null);
+        }
     }
 }
